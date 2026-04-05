8f8f73da5e913e633dcf3db3a57269b0800861ee
Nabajyoti Majumdar <52462493+blackenwhite@users.noreply.github.com>
2026-04-05 12:41:08 +0530

    Create Anna-and-the-Valentine's-Day-Gift.cpp

---
+++ 1400/Anna-and-the-Valentine's-Day-Gift.cpp
@@ -0,0 +1,156 @@
+/**
+ * problem:
+ * author: Nabajyoti
+ */
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+#define ff first
+#define ss second
+
+using ll = long long int;
+using pii = pair<ll, ll>;
+const ll N = 1e5 + 5;
+const ll mod = 1e9 + 7;
+ll inf = 1e18;
+using vl = vector<ll>;
+
+template <typename T>
+class LazySGT {
+   private:
+    const ll sz;
+    vector<T> tree;
+    vector<T> lazy;
+
+    void build(int v, int l, int r, const vector<T>& a) {
+        if (l == r) {
+            tree[v] = a[r];
+            return;
+        }
+
+        ll m = (l + r) / 2;
+        build(2 * v, l, m, a);
+        build(2 * v + 1, m + 1, r, a);
+        tree[v] = merge(tree[2 * v], tree[2 * v + 1]);
+    }
+
+    /** applies lazy update to tree[v], places update at lazy[v] */
+    void apply(int v, int len, const T mult) {
+        tree[v] = (tree[v] * mult) % mod;
+        lazy[v] = (lazy[v] * mult) % mod;
+    }
+
+    T merge(T a, T b) { return (a + b) % mod; }
+
+    void push_down(int v, int l, int r) {
+        int m = (l + r) / 2;
+        apply(2 * v, m - l + 1, lazy[v]);
+        apply(2 * v + 1, r - m, lazy[v]);
+        lazy[v] = 1;
+    }
+
+    void range_update(int v, int l, int r, int ql, int qr, const T& x) {
+        if (qr < l || ql > r) return;
+        if (l >= ql && r <= qr) {
+            apply(v, r - l + 1, x);
+            return;
+        }
+        push_down(v, l, r);
+        int m = (l + r) / 2;
+        range_update(2 * v, l, m, ql, qr, x);
+        range_update(2 * v + 1, m + 1, r, ql, qr, x);
+        tree[v] = merge(tree[2 * v], tree[2 * v + 1]);
+    }
+
+    T range_query(int v, int l, int r, int ql, int qr) {
+        if (qr < l || ql > r) return 0;
+        if (l >= ql && r <= qr) return tree[v];
+        push_down(v, l, r);
+        int m = (l + r) / 2;
+        return merge(range_query(2 * v, l, m, ql, qr), range_query(2 * v + 1, m + 1, r, ql, qr));
+    }
+
+   public:
+    LazySGT(const vector<T>& a) : sz(a.size()), tree(4 * sz), lazy(4 * sz, 1) {
+        build(1, 0, sz - 1, a);
+    }
+
+    void range_update(int ql, int qr, T add) { range_update(1, 0, sz - 1, ql, qr, add); }
+
+    T range_sum(int ql, int qr) { return range_query(1, 0, sz - 1, ql, qr); }
+
+    void printTree() {
+        cout << "Printing tree ---> \n";
+        for (ll i = 1; i < tree.size(); i++) {
+            cout << tree[i] << "(" << lazy[i] << ")" << ", ";
+        }
+        cout << "\n";
+    }
+};
+
+// ----- end of utilities ------ //
+
+ll n, m;
+
+void solve() {
+    ll total_digits = 0;
+    cin >> n >> m;
+    vl v;
+
+    for (ll i = 0; i < n; i++) {
+        ll x;
+        cin >> x;
+
+        // first count the trailing zeros
+        ll trailing_zeros = 0;
+        while (x % 10 == 0) {
+            trailing_zeros++;
+            total_digits++;
+            x /= 10;
+        }
+
+        while (x) {
+            total_digits++;
+            x /= 10;
+        }
+
+        v.push_back(trailing_zeros);
+    }
+
+    sort(v.begin(), v.end());
+    reverse(v.begin(), v.end());
+
+    for (ll i = 0; i < v.size(); i += 2) {
+        total_digits -= v[i];
+    }
+
+    if (total_digits > m) {
+        cout << "Sasha\n";
+    } else {
+        cout << "Anna\n";
+    }
+}
+
+signed main() {
+    ll t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
